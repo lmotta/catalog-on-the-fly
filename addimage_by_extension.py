b5aabf5f45c9b47f9c1ee7ec3d31e774fc1fd6ae
Luiz Motta <motta.luiz@gmail.com>
2015-03-19 12:11:36 -0300

    Refactoring: change class Highlight - BUG Zoom continue

--- addimage_by_extension.py
+++ addimage_by_extension.py
@@ -1,25 +1,27 @@
-#
+# -*- coding: utf-8 -*-
+
+
 import urllib2
 from os.path import basename
 
-from PyQt4.QtCore import *
+from PyQt4.QtCore import ( QTimer, QFileInfo, Qt )
+from qgis.gui import ( QgsHighlight, QgsMessageBar ) 
+from qgis.core import (
+  QgsProject,
+  QgsMapLayerRegistry, QgsMapLayer,
+  QgsFeature, QgsFeatureRequest, QgsGeometry,
+  QgsCoordinateTransform,
+  QgsRasterLayer, QgsRasterTransparency,
+)
+from qgis.utils import ( iface )
 
-from qgis.gui import *
-from qgis.core import *
-from qgis import utils
+class FeatureImage:
 
-class HighlightFeature:
   def __init__(self, layer):
     self.layer = layer
-    self.msgBar = utils.iface.messageBar()
-    self.hl = self.geom = self.width = None
-  #
-  def _changeWidth(self):
-    if self.hl is None:
-      return
-    self.hl.setWidth( self.width )
-    self.hl.update()
-  #
+    self._image = self.geom = self.hl = self.msgError = None
+    self.canvas = iface.mapCanvas()
+
   def _getGeometry(self, fid):
     fr = QgsFeatureRequest( fid )
     fr.setSubsetOfAttributes( [], self.layer.dataProvider().fields() )
@@ -27,60 +29,81 @@ class HighlightFeature:
     feat = QgsFeature()
     isOk = it.nextFeature( feat )
     it.close()
-    #
+
     return QgsGeometry( feat.geometry() ) if isOk else None
-  #
-  def highlight(self, fid, second=0 ):
-    self.geom = self.calcGeometry( fid )
+
+  def setImage(self, image=None, dicImages=None):
+    if image is None and dicImages is None:
+      self._image = None
+      del self.geom
+      self.geom = None
+      return True 
+    #
+    if self._image == image:
+      return True
+    #
+    fid = dicImages[ image ]['id']
+    geom = self._getGeometry( fid )
+    if geom is None:
+      self.msgError = "Feature (fid = %d) of layer ('%s') is invalid" % ( fid, self.layer.name() )
+      return False
+    #
+    self.geom = geom
+    self._image = image
+    #
+    del self.hl
+    self.hl = QgsHighlight( self.canvas, self.geom, self.layer )
+    self.hl.hide()
+    #
+    return True
+
+  def image(self):
+    return self._image
+
+  def highlight(self, second=0 ):
+
+    if self.hl is None:
+      return
+    #
+    self.hl.setWidth( 5 )
+    self.hl.show()
+    #
+    QTimer.singleShot( second * 1000, self.hl.hide )
+
+  def zoom(self):
     if self.geom is None:
       return
     #
-    if not self.hl is None:
-      del self.hl
+    crsCanvas = self.canvas.mapSettings().destinationCrs()
+    crsLayer = self.layer.crs()
+    ct = QgsCoordinateTransform( crsCanvas, crsLayer )
+    extent = self.geom.boundingBox() if crsCanvas == crsLayer else ct.transform( self.geom.boundingBox() )
     #
-    self.hl = QgsHighlight( utils.iface.mapCanvas(), self.geom, self.layer )
-    if second <> 0:
-      self.width = 6
-      self._changeWidth()        
-      self.width = 1
-      QTimer.singleShot( second * 1000, self._changeWidth )
-  #
-  def geometry(self):
-    return self.geom
-  #
-  def calcGeometry(self, fid):
-    geom = self._getGeometry( fid )
-    if geom is None:
-      msg = "Feature (fid = %d) of layer ('%s') is invalid" % ( fid, self.layer.name() )
-      self.msgBar.pushMessage( msg, QgsMessageBar.CRITICAL, 4 )
-      return None
-    else:
-      return geom
-  #
-  def clean(self):
-    if not self.hl is None:
-      del self.hl
-      self.hl = None
-      self.geom = None
-#
+    self.canvas.setExtent( extent )
+    self.layer.triggerRepaint()
+
+  def msgError(self):
+    return self.msgError
+
+
 class CatalogOTF:
+
   def __init__(self):
-    self.canvas = utils.iface.mapCanvas()
-    self.layer = self.nameFieldSource = self.nameFieldDate =  None
-    self.ltgCatalog = self.dicImage = None
-    self.hlFeature = self.currentImage= None
-    self.zoomImage = False
-    self.highlightImage = False
-    self.msgBar = utils.iface.messageBar()
+    self.canvas = iface.mapCanvas()
+    self.ltv = iface.layerTreeView()
+    self.msgBar = iface.messageBar()
     self.tempDir = "/tmp"
-  #
+    self.layer = self.nameFieldSource = self.nameFieldDate =  None
+    self.ltgCatalog = self.dicImages = None
+    self.featureImage = None
+    self.zoomImage = self.highlightImage = False
+
   def _connect(self, isConnect = True):
-    ltv = utils.iface.layerTreeView()
     signal_slot = [
       { 'signal': self.canvas.extentsChanged , 'slot': self.onExtentsChangedMapCanvas },
       { 'signal': self.canvas.destinationCrsChanged, 'slot': self.onDestinationCrsChanged_MapUnitsChanged },
       { 'signal': self.canvas.mapUnitsChanged, 'slot': self.onDestinationCrsChanged_MapUnitsChanged },
-      { 'signal': ltv.currentLayerChanged , 'slot': self.onCurrentLayerChanged  }
+      { 'signal': self.ltv.currentLayerChanged , 'slot': self.onCurrentLayerChanged  }
     ]
     if isConnect:
       for item in signal_slot:
@@ -88,8 +111,31 @@ class CatalogOTF:
     else:
       for item in signal_slot:
         item['signal'].disconnect( item['slot'] )  
-  #
+
+  def _setFeatureImage(self, layer):
+    if layer is None or \
+       layer.type() != QgsMapLayer.RasterLayer or \
+       self.ltgCatalog is None or \
+       self.ltgCatalog.findLayer( layer.id() ) is None:
+      #
+      return False
+    #
+    image = basename( layer.source() )
+    if not image in self.dicImages .keys():
+      msg = "Image (%s) not in catalog layer ('%s')" % ( image, self.layer.name() )
+      self.msgBar.pushMessage( msg, QgsMessageBar.CRITICAL, 4 )
+      #
+      return False
+    #
+    if not self.featureImage.setImage( image, self.dicImages ):
+      self.msgBar.pushMessage( self.featureImage.msgError(), QgsMessageBar.CRITICAL, 4 )
+      #
+      return False
+    #
+    return True
+
   def onExtentsChangedMapCanvas(self):
+
     def getImagesByCanvas():
       images = []
       #
@@ -112,19 +158,23 @@ class CatalogOTF:
         images.append( basename( f[ self.nameFieldSource ] ) )
       #
       return images
-    #
+
     def addImages(images):
+
       def addImage(image):
+
         def _addImage():
+
           def setTransparence():
+
             def getTTVP():
               ts = QgsRasterTransparency.TransparentThreeValuePixel()
               ts.red = ts.green = ts.blue = 0.0
               ts.percentTransparent = 100.0
               return ts
-            #
+
             layerImage.renderer().rasterTransparency().setTransparentThreeValuePixelList( [ getTTVP() ] )
-          #
+
           layerImage = QgsRasterLayer( fileInfo.filePath(), fileInfo.baseName() )
           if not layerImage.isValid():
             return False
@@ -142,8 +192,9 @@ class CatalogOTF:
             name = "%s (%s)" % ( date.toString( "yyyy-MM-dd" ), image )
             ltl.setLayerName( name )
             return True
-        #
+
         def prepareFileTMS( url_tms ):
+
           def createLocalFile():
             response = urllib2.urlopen( url_tms )
             html = response.read()
@@ -152,7 +203,7 @@ class CatalogOTF:
             fw = open( localName, 'w' )
             fw.write( html )
             fw.close()
-          #
+
           localName = "%s/%s" % ( self.tempDir, basename( url_tms ) )
           fileInfo = QFileInfo( localName )
           #
@@ -161,152 +212,125 @@ class CatalogOTF:
             fileInfo = QFileInfo( localName )
           #
           return fileInfo
-        #
-        value = self.dicImage [image]
+
+        value = self.dicImages [image]
         source = value['source']
         date = value['date']
         fileInfo = prepareFileTMS( source ) if not source.find('http://') == -1 else QFileInfo( source )
         #
         return _addImage()
-      #
+
       def getSortedImages(images, v_reverse=False):
-        images_date = map( lambda item: { 'image': item, 'date': self.dicImage [ item ]['date'] }, images )
+        images_date = map( lambda item: { '_image': item, 'date': self.dicImages [ item ]['date'] }, images )
         return sorted( images_date, key = lambda item: item['date'], reverse = v_reverse ) 
-      #
+
       l_error = []
       for item in getSortedImages( images, True ):
-        if not addImage( item['image'] ):
-          l_error.append( item['image'] )
+        if not addImage( item['_image'] ):
+          l_error.append( item['_image'] )
       if len( l_error ) > 0:
         msg = "\n" .join( l_error )
         self.msgBar.pushMessage( "Images invalid:\n%s" % msg, QgsMessageBar.CRITICAL, 5 )
         del l_error[:]
       else:
         msg = "%s - Total %d" % ( self.ltgCatalog.name(), len( images ) )
-        utils.iface.mainWindow().statusBar().showMessage( msg )
-    #
-    def setCurrentImage( ):
-      sourceImage = self.dicImage[ self.currentImage ]['source']
+        iface.mainWindow().statusBar().showMessage( msg )
+
+    def setCurrentImage():
+      sourceImage = self.dicImages[ self.featureImage.image() ]['source']
       ltlsImage = filter( lambda item: item.layer().source() == sourceImage, self.ltgCatalog.findLayers()  )
       if len( ltlsImage ) > 0:
-        ltv.setCurrentLayer( ltlsImage[0].layer() )
-        if self.highlightImage:
-          self.hlFeature.highlight( self.dicImage [ self.currentImage ]['id'], 3 )
-      else:
-        self.hlFeature.clean()
-    #
+        self.ltv.setCurrentLayer( ltlsImage[0].layer() )
+
     if self.layer is None:
       self.msgBar.pushMessage( "Need define layer catalog", QgsMessageBar.WARNING, 2 )
       return
     #
-    ltv = utils.iface.layerTreeView()
-    #
-    prevFlag = self.canvas.renderFlag()
-    self.canvas.setRenderFlag( False )
-    signal_slot = { 'signal': ltv.currentLayerChanged , 'slot': self.onCurrentLayerChanged }
+    signal_slot = { 'signal': self.ltv.currentLayerChanged , 'slot': self.onCurrentLayerChanged }
     signal_slot['signal'].disconnect( signal_slot['slot'] )
     #
     self.ltgCatalog.removeAllChildren()
     #
     addImages( getImagesByCanvas() )
     #
-    if not self.currentImage is None:
+    if not self.featureImage.image() is None:
       setCurrentImage() 
     #    
     signal_slot['signal'].connect( signal_slot['slot'] )
-    self.canvas.setRenderFlag( prevFlag )
-    self.canvas.refresh()
-  #
-  def onCurrentLayerChanged(self, rasterLayer):
-    if not self.highlightImage and not self.zoomImage :
+
+  def onCurrentLayerChanged(self, layer ):
+    if layer is None or not self.highlightImage and not self.zoomImage :
       return
     #
-    self.currentImage = None
+    self.featureImage.setImage() # Clear
     #
-    if rasterLayer is None or \
-       rasterLayer.type() != QgsMapLayer.RasterLayer or \
-       self.ltgCatalog is None or \
-       self.ltgCatalog.findLayer( rasterLayer.id() ) is None:
-      #
-      self.hlFeature.clean()
+    if not self._setFeatureImage( layer ): 
       return
-    #
-    image = basename( rasterLayer.source() )
-    if not image in self.dicImage .keys():
-      msg = "Image (%s) not in catalog layer ('%s')" % ( image, self.layer.name() )
-      self.msgBar.pushMessage( msg, QgsMessageBar.CRITICAL, 4 )
-      return
-    #
-    self.currentImage = image
     #
     if self.highlightImage:
-      self.hlFeature.highlight( self.dicImage[ self.currentImage ]['id'], 3 )
+      self.featureImage.highlight( 3 )
     #
     if self.zoomImage:
-      geom = self.hlFeature.geometry()
-      #
-      if geom is None:
-        geom = self.hlFeature.calcGeometry( self.dicImage [ self.currentImage ]['id'] )
-        if geom is None:
-          return
-      #
-      self.canvas.setExtent( geom.boundingBox() )
-    #
-  #
+      self.featureImage.zoom()
+
   def onDestinationCrsChanged_MapUnitsChanged(self):
     self.onExtentsChangedMapCanvas()
-  #
+
   def setLayerCatalog(self, layer, nameFieldSource, nameFieldDate ):
-    def setDicSourceDate():
+
+    def setDicImages():
       fr = QgsFeatureRequest()
       fieldsRequest = [ self.nameFieldSource, self.nameFieldDate ]
       fr.setSubsetOfAttributes( fieldsRequest, layer.dataProvider().fields() )
       fr.setFlags( QgsFeatureRequest.NoGeometry )
       #
-      self.dicImage  = {}
+      self.dicImages  = {}
       it = self.layer.getFeatures( fr )
       f = QgsFeature()
       while it.nextFeature( f ):
         key = basename( f[ self.nameFieldSource ] )
         value = { 'source': f[ self.nameFieldSource ], 'date': f[ self.nameFieldDate ], 'id': f.id() }
-        self.dicImage [key] = value
+        self.dicImages [key] = value
       it.close()
-    #
+
     self.layer = layer
-    self.hlFeature = HighlightFeature( layer )
+    self.featureImage = FeatureImage( layer )
     self.nameFieldSource = nameFieldSource
     self.nameFieldDate = nameFieldDate
-    setDicSourceDate()
-  #
+    setDicImages()
+
   def enable( self, onEnabled=True ):
+
     def setGroupCatalog():
       nameCatalogGroup = "%s - Catalog" % self.layer.name()
       root = QgsProject.instance().layerTreeRoot()
       self.ltgCatalog = root.findGroup( nameCatalogGroup )
       if self.ltgCatalog is None:
         self.ltgCatalog = root.addGroup( nameCatalogGroup )
-    #
+
     self._connect( onEnabled )
     if onEnabled:
       setGroupCatalog()
       self.onExtentsChangedMapCanvas()
-    else:
-      self.hlFeature.clean()
-      self.ltgCatalog = None
-  #
+
   def onZoomImage(self, on=True):
     self.zoomImage = on
-  #
+    if on and self._setFeatureImage( self.ltv.currentLayer() ): 
+      self.featureImage.zoom()
+
   def onHighlightImage(self, on=True):
     self.highlightImage = on
-    if not on:
-      self.hlFeature.clean()
-  #
-#
+    if on and self._setFeatureImage( self.ltv.currentLayer() ):
+      self.featureImage.highlight( 3 )
+
+
 def init():
   layer = iface.mapCanvas().currentLayer()
   if layer is None:
     print "Selecione o layer de catalogo"
+    return None
+  if layer.type() == QgsMapLayer.RasterLayer:
+    print u"Layer selecione é do tipo RASTER, selecione o layer de catalogo"
     return None
   cotf = CatalogOTF(); cotf.setLayerCatalog( layer, "address", "data" )
   cotf.enable()
@@ -315,7 +339,7 @@ def init():
 #
 #
 """
-execfile(u'/home/lmotta/data/qgis_script_console/addimage_by_extension.py'.encode('UTF-8')); cotf = init()
+execfile(u'/home/lmotta/data/qgis_script_console/addimage_by_extension/addimage_by_extension.py'.encode('UTF-8')); cotf = init()
 cotf.onHighlightImage(); cotf.onZoomImage()
 cotf.enable( False ); del cotf; cotf = None
 """
